c65e010a980f60af11126aa4a02d627b63f56839
Kiy Hong Tang <kiyhong@gmail.com>
2014-03-13 08:50:50 +0000

    added player class

---
+++ lib/player.rb
@@ -0,0 +1,20 @@
+require "board"
+
+class Player
+
+  attr_reader :board
+
+  def initialize(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def afloat
+      
+
+    
+  end
+end
---
+++ spec/player_spec.rb
@@ -0,0 +1,24 @@
+require "player"
+require "board"
+
+describe Player do
+  
+  let(:player) {Player.new("Laura")}
+
+  it "should initialize with a name" do
+  	expect(player.name).to eq "Laura"  	
+  end
+
+  it "should know that there are still ships that are not hit" do
+  	player.afloat.to eq true
+  end
+
+
+end
+
+
+
+  # Tells us if there are still ships that have not been hit
+  # on her/his board.
+
+  # Shoots at the opponents board.
